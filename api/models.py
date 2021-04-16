7ecf1e1bbe9851852e4c694ac560095329cfce1d
roman <roman@mac-pro.local>
2021-04-16 23:22:20 +0600

    update

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+
+class Note(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    title = models.CharField(max_length=256)
+    note = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- api/views.py
+++ api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import NoteSerializer
+from .models import Note
 
 # Create your views here.
+
+"""
+API Overview
+"""
+
+
+@api_view(['GET'])
+def api_overview(request):
+    urls = {
+        'List': '/note-list/',
+        'Detail View': '/note-detail/<str:pk>/',
+        'Create': '/note-create/',
+        'Update': '/note-update/<str:pk>/',
+        'Delete': '/note-delete/<str:pk>/'
+    }
+    return Response(urls)
+
+"""
+Below Function going to display all the notes stored in the data base.
+"""
+@api_view(['GET'])
+def note_list(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
